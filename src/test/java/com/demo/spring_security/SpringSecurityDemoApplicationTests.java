4dcafe09344d161977d0bf8a57fd030fa060e7f1
Md. Zohir Raihan <zohir@erainfotechbd.com>
2024-03-26 14:58:36 +0600

    Initial Project Structure

---
+++ src/test/java/com/demo/spring_security/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
